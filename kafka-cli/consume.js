66da1ca9eb5099b3a6f5656377df6e963d014c2b
Mark Green <markosindustries@gmail.com>
2018-09-04 21:47:15 +1000

    Use single line output to make piping more useful (especially grepping)

--- kafka-cli/consume.js
+++ kafka-cli/consume.js
@@ -12,6 +12,14 @@ const offsetLookup = {
 }
 
 async function consume(kafkaBrokers, protobufPath, topics, schemaName, seekType, timestamp) {
+  if(process.stdout.isTTY) {
+    console.table({
+      mode: 'consume',
+      schema: schemaName,
+      topics: topics.join(', '),
+    })
+  }
+
   const kafkaClient = await kafka.getKafkaClient(kafkaBrokers)
   const topicPartitionOffsets = await offsetLookup[seekType](kafkaClient, topics, timestamp)
   const schema = protobuf.loadDirectory(protobufPath).lookupType(schemaName)
@@ -21,7 +29,14 @@ async function consume(kafkaBrokers, protobufPath, topics, schemaName, seekType,
   process.once('SIGTERM', () => keepGoing = false)
 
   await kafka.consumeFromOffsets(kafkaClient, topicPartitionOffsets, (message) => {
-    console.log(schema.decode(message.value).toJSON())
+    const jsonMessage = schema.decode(message.value).toJSON()
+    console.log({
+        partition:message.partition,
+        offset:message.offset,
+        utc_time:message.timestamp,
+      },
+      JSON.stringify(jsonMessage)// TODO: can I do single line but keep the nice highlighting of console.log(jsonMessage) ?
+    )
     return keepGoing
   })
   kafkaClient.close()
