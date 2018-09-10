0c6fc1430d3594a0d7d0f8251f6090eb2adfea9e
Mark Green <markosindustries@gmail.com>
2018-09-10 17:45:01 +1000

    Handle seek types and timestamps out of range

--- kat/src/main/kotlin/kat/Consume.kt
+++ kat/src/main/kotlin/kat/Consume.kt
@@ -2,15 +2,17 @@ package kat
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.default
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
-import kat.kafka.Brokers
-import kat.kafka.EarliestOffsetSpec
-import kat.kafka.EphemeralConsumer
+import kat.kafka.*
 import java.nio.ByteBuffer
+import java.time.Instant
 
 class Consume() : CliktCommand(help = "Consume records from Kafka") {
-  val topic by argument("topic")
+  val topic by argument("topic", "which topic should be consumed")
+  val seek by argument("seek", "one of 'earliest', 'latest', or 'timestamp'").default("earliest")
+  val timestamp by argument("timestamp", "the epoch milliseconds timestamp to seek to").default(Instant.now().toEpochMilli().toString())
   val brokers by option(envvar = "KAFKA_BROKERS").default("localhost:9092")
   val encoding by option()
 
@@ -20,7 +22,14 @@ class Consume() : CliktCommand(help = "Consume records from Kafka") {
     val sizeBufferArray = ByteArray(4)
     val sizeBuffer: ByteBuffer = ByteBuffer.wrap(sizeBufferArray)
 
-    ephemeralConsumer.consumeFrom(topic, EarliestOffsetSpec()) { record ->
+    val offsetSpec = when(seek.trim().toLowerCase()) {
+      "earliest" -> EarliestOffsetSpec()
+      "latest" -> LatestOffsetSpec()
+      "timestamp" -> TimestampOffsetSpec(timestamp.toLong())
+      else -> throw RuntimeException("Unexpected seek option - $seek")
+    }
+
+    ephemeralConsumer.consumeFrom(topic, offsetSpec) { record ->
       // TODO: move swithcing to a lambda return func
       when(encoding) {
         "hex" -> {
--- kat/src/main/kotlin/kat/kafka/TimestampOffsetSpec.kt
+++ kat/src/main/kotlin/kat/kafka/TimestampOffsetSpec.kt
@@ -5,8 +5,16 @@ import org.apache.kafka.common.TopicPartition
 
 class TimestampOffsetSpec(val timestampEpochMs: Long) : OffsetSpec {
   override fun <K,V>getOffsets(consumer:Consumer<K,V>, topicPartitions: List<TopicPartition>): Map<TopicPartition, Long> {
+    val latestOffsets = consumer.endOffsets(topicPartitions.toList())
+
     return consumer
       .offsetsForTimes(topicPartitions.map { it to timestampEpochMs }.toMap())
-      .mapValues { it.value.offset() }
+      .mapValues {
+        if(it.value != null) {
+          it.value.offset()
+        } else {
+          latestOffsets[it.key]!!
+        }
+      }
   }
 }
