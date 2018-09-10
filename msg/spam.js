453c1dda02e13d9264097838a567b28decedd020
Mark Green <markosindustries@gmail.com>
2018-09-11 07:53:53 +1000

    Use filter as more of an override for spam mode
    Rather than waiting for randomly matching values, just use them every time.

--- msg/spam.js
+++ msg/spam.js
@@ -8,7 +8,7 @@ module.exports = {
 
 function spam(schemaName, delimiterBuffer, protobufPath, filterJsonObject) {
   const schema = protobuf.loadDirectory(protobufPath).lookupType(schemaName)
-console.log(filterJsonObject)
+
 // TODO extract this - it's in 3 files already
   const serialiseJsonObject = process.stdout.isTTY ? x=>inspect(x, {
     colors: true,
@@ -16,11 +16,9 @@ console.log(filterJsonObject)
   }) : x=>JSON.stringify(x)
 
   while(true) {
-    const jsonObject = protobuf.makeValidJsonRecord(schema)
-    if(matchesFilter(jsonObject, filterJsonObject)) {
-      process.stdout.write(serialiseJsonObject(jsonObject))
-      process.stdout.write(delimiterBuffer)
-    }
+    const jsonObject = Object.assign(protobuf.makeValidJsonRecord(schema), filterJsonObject)
+    process.stdout.write(serialiseJsonObject(jsonObject))
+    process.stdout.write(delimiterBuffer)
   }
 
   process.stdin.on('end', () => { process.exit() })
