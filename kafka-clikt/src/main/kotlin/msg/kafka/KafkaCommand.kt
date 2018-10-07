e6dc015d723ea866faa4b91b8106fb9fc7bd8315
Mark Green <markosindustries@gmail.com>
2018-10-08 08:25:11 +1100

    Fixed typo

--- kafka-clikt/src/main/kotlin/msg/kafka/KafkaCommand.kt
+++ kafka-clikt/src/main/kotlin/msg/kafka/KafkaCommand.kt
@@ -16,7 +16,7 @@ abstract class KafkaCommand(help:String) : CliktCommand(help) {
   private val brokers by option("--brokers", "-b", help = "comma separated list of broker addresses",envvar = "KAFKA_BROKERS").default("localhost:9092")
   private val protocol by option("--protocol", "-p", help = "the security mechanism to use to connect\nAny SASL-based protocol will require a SASL mechanism", envvar="KAFKA_PROTOCOL")
     .choice(*SecurityProtocol.names().toTypedArray()).default(SecurityProtocol.PLAINTEXT.toString())
-  private val sasl by option("--sasl", "-a", help = "SASL mechanism to use for authentication. eg: SCRAM-SHA=256", envvar = "KAFKA_SASL").default("")
+  private val sasl by option("--sasl", "-a", help = "SASL mechanism to use for authentication. eg: SCRAM-SHA-256", envvar = "KAFKA_SASL").default("")
 
   fun <K,V,DK:Deserializer<K>,DV:Deserializer<V>>newConsumer(keyDeserialiser: KClass<DK>, valueDeserialiser: KClass<DV>, vararg config: Pair<String,Any>): Consumer<K,V> =
     EphemeralConsumer(
