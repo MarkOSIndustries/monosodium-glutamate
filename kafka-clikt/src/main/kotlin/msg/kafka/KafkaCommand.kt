1833d0c041ffdc51faca77817bff736bb5a561c6
Mark Green <markosindustries@gmail.com>
2018-10-15 17:44:47 +1100

    Add cmd-line supplied JAAS config support

--- kafka-clikt/src/main/kotlin/msg/kafka/KafkaCommand.kt
+++ kafka-clikt/src/main/kotlin/msg/kafka/KafkaCommand.kt
@@ -17,6 +17,7 @@ abstract class KafkaCommand(help:String) : CliktCommand(help) {
   private val protocol by option("--protocol", "-p", help = "the security mechanism to use to connect\nAny SASL-based protocol will require a SASL mechanism", envvar="KAFKA_PROTOCOL")
     .choice(*SecurityProtocol.names().toTypedArray()).default(SecurityProtocol.PLAINTEXT.toString())
   private val sasl by option("--sasl", "-a", help = "SASL mechanism to use for authentication. eg: SCRAM-SHA-256", envvar = "KAFKA_SASL").default("")
+  private val jaas by option("--jaas", "-j", help = "JAAS configuration - useful when you need to use kat against two different environments with different authentication").default("")
 
   fun <K,V,DK:Deserializer<K>,DV:Deserializer<V>>newConsumer(keyDeserialiser: KClass<DK>, valueDeserialiser: KClass<DV>, vararg config: Pair<String,Any>): Consumer<K,V> =
     EphemeralConsumer(
@@ -25,6 +26,7 @@ abstract class KafkaCommand(help:String) : CliktCommand(help) {
       valueDeserialiser,
       CommonClientConfigs.SECURITY_PROTOCOL_CONFIG to protocol,
       "sasl.mechanism" to sasl,
+      "sasl.jaas.config" to jaas,
       *config
     )
 
@@ -35,6 +37,7 @@ abstract class KafkaCommand(help:String) : CliktCommand(help) {
     "monosodium-glutamate",
     CommonClientConfigs.SECURITY_PROTOCOL_CONFIG to protocol,
     "sasl.mechanism" to sasl,
+    "sasl.jaas.config" to jaas,
     *config
   )
 }
