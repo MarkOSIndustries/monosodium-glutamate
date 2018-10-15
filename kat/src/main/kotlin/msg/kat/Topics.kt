7538dba1c0244a669f6866b7fbf29006a1c1eafa
Mark Green <markosindustries@gmail.com>
2018-10-15 18:19:26 +1100

    Add extra metadata option to topics command

--- kat/src/main/kotlin/msg/kat/Topics.kt
+++ kat/src/main/kotlin/msg/kat/Topics.kt
@@ -5,12 +5,16 @@ import com.github.ajalt.clikt.parameters.arguments.default
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import msg.kafka.KafkaCommand
+import msg.kafka.offsets.EarliestOffsetSpec
+import msg.kafka.offsets.LatestOffsetSpec
+import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.serialization.ByteArrayDeserializer
 import java.time.Duration
 
 class Topics : KafkaCommand("Query topics\nRetrieves the names of all topics and prints to stdout") {
   private val query by argument(help = "Filter the topic list (case-insensitive, supports * wildcards)").default("*")
   private val not by option("--not", "-n", help = "Invert the filter to be exclusive rather than inclusive").flag(default = false)
+  private val offsets by option("--offsets", "-o", help = "Include offset ranges for each topic").flag(default = false)
 
   override fun run() {
     val pattern = ".*${query.split("*").map { Regex.escape(it) }.joinToString(".*")}.*"
@@ -20,6 +24,17 @@ class Topics : KafkaCommand("Query topics\nRetrieves the names of all topics and
     consumer.listTopics(Duration.ofMinutes(1))
       .filterKeys { if(not) !regex.matches(it) else regex.matches(it) }
       .toSortedMap()
-      .forEach { println(it.key) }
+      .forEach { topicWithPartitionInfos ->
+        println(topicWithPartitionInfos.key)
+        if(offsets) {
+          val topicPartitionsWithInfo = topicWithPartitionInfos.value.map { TopicPartition(it.topic(), it.partition()) to it }.toMap()
+          val earliest = EarliestOffsetSpec().getOffsets(consumer, topicPartitionsWithInfo.keys)
+          val latest = LatestOffsetSpec().getOffsets(consumer, topicPartitionsWithInfo.keys)
+          earliest.forEach {
+            val topicPartitionInfo = topicPartitionsWithInfo[it.key]!!
+            println("  ${it.key} | Offset range [${it.value}, ${latest[it.key]}) | ISR <${topicPartitionInfo.inSyncReplicas().joinToString(",")}> | Leader ${topicPartitionInfo.leader()}")
+          }
+        }
+      }
   }
 }
