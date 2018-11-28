f12c8ee9e52cd1bbb778b2b42679ec67b3c3d39c
Mark Green <markosindustries@gmail.com>
2018-11-28 16:51:50 +1100

    Expand DNS names to a list of addresses if possible
    Allow for DNS names which refer to the whole cluster to still tolerate node outages.

--- kafka/src/main/kotlin/msg/kafka/Brokers.kt
+++ kafka/src/main/kotlin/msg/kafka/Brokers.kt
@@ -2,12 +2,22 @@ package msg.kafka
 
 object Brokers {
   fun from(csvBrokers: String):List<Broker> =
-    csvBrokers.split(",").map { broker ->
-      val hostAndPort = broker.split(":").map { it.trim() }
-      if(hostAndPort.size == 1) {
-        Broker(hostAndPort[0], 9092)
-      } else {
-        Broker(hostAndPort[0], hostAndPort[1].toInt())
+    csvBrokers
+      .split(",")
+      .map { broker ->
+        val hostAndPort = broker.split(":").map { it.trim() }
+        if(hostAndPort.size == 1) {
+          Broker(hostAndPort[0], 9092)
+        } else {
+          Broker(hostAndPort[0], hostAndPort[1].toInt())
+        }
       }
-    }
+      .flatMap { broker ->
+        val addresses = java.net.InetAddress.getAllByName(broker.host)
+        if(addresses.isEmpty()) {
+          listOf(broker)
+        } else {
+          addresses.map { address -> Broker(address.hostAddress, broker.port) }
+        }
+      }
 }
