56ad49fa59735f58b1b0afae6e25383936c58722
Mark Green <markosindustries@gmail.com>
2020-07-29 14:20:36 +1000

    Write integers as little endian to rocksdb

--- qs/src/main/kotlin/msg/qs/Uint64Serialisation.kt
+++ qs/src/main/kotlin/msg/qs/Uint64Serialisation.kt
@@ -1,17 +1,20 @@
 package msg.qs
 
 import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 interface Uint64Serialisation {
   companion object {
-    fun longToByteArray(l: Long): ByteArray? {
+    fun longToByteArray(l: Long): ByteArray {
       val buf: ByteBuffer = ByteBuffer.allocate(java.lang.Long.BYTES)
+      buf.order(ByteOrder.LITTLE_ENDIAN)
       buf.putLong(l)
       return buf.array()
     }
 
     fun longFromByteArray(a: ByteArray): Long {
       val buf: ByteBuffer = ByteBuffer.allocate(java.lang.Long.BYTES)
+      buf.order(ByteOrder.LITTLE_ENDIAN)
       buf.put(a)
       buf.flip()
       return buf.getLong()
