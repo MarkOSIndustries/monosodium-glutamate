9f7a989188332652c15e700ed517b6046632c988
Mark Green <markosindustries@gmail.com>
2021-05-18 23:52:02 +1000

    Added basic completion support for kat

--- kafka-clikt/src/main/kotlin/msg/kafka/nonSplittingOption.kt
+++ kafka-clikt/src/main/kotlin/msg/kafka/nonSplittingOption.kt
@@ -1,6 +1,8 @@
 package msg.kafka
 
+import com.github.ajalt.clikt.completion.CompletionCandidates
 import com.github.ajalt.clikt.parameters.options.NullableOption
+import com.github.ajalt.clikt.parameters.options.ValueWithDefault
 import com.github.ajalt.clikt.parsers.OptionWithValuesParser
 
 fun nonSplittingOption(
@@ -8,18 +10,22 @@ fun nonSplittingOption(
   help: String = "",
   metavar: String? = null,
   hidden: Boolean = false,
-  envvar: String? = null
+  envvar: String? = null,
+  completionCandidates: CompletionCandidates? = null
 ) = NullableOption(
   names = names.toSet(),
-  metavarExplicit = metavar,
-  metavarDefault = "TEXT",
+  metavarWithDefault = ValueWithDefault(metavar, "TEXT"),
   nvalues = 1,
   help = help,
   hidden = hidden,
   envvar = envvar,
-  envvarSplit = Regex("^$"), // hopefully that never happens
+  envvarSplit = ValueWithDefault(null, Regex("^$")), // hopefully that never happens
   parser = OptionWithValuesParser,
   transformValue = { it },
   transformEach = { it.single() },
-  transformAll = { it.joinToString(" ") }
+  transformAll = { it.joinToString(" ") },
+  helpTags = emptyMap(),
+  completionCandidatesWithDefault = ValueWithDefault(completionCandidates, CompletionCandidates.Path),
+  valueSplit = null,
+  transformValidator = {}
 )
