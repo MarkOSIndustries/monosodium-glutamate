b969807b1c35245925d80bf92c6ab5c8c875e836
Mark Green <markosindustries@gmail.com>
2021-05-18 22:00:30 +1000

    Add a command to kat to partition keys using the default strategy
    Makes it easier to figure out which partition you need to scan for a given record if you know the partition key

---
+++ kat/src/main/kotlin/msg/kat/Partition.kt
@@ -0,0 +1,29 @@
+package msg.kat
+
+import msg.kafka.topicPartitions
+import org.apache.kafka.common.serialization.ByteArrayDeserializer
+import org.apache.kafka.common.utils.Utils
+import java.io.EOFException
+import java.time.Duration
+
+class Partition : KafkaTopicDataCommand(
+  "Apply the default Java Kafka key partitioning strategy.\n\n" +
+    "Reads keys from stdin and writes the topic partition they would be produced to by default to stdout"
+) {
+
+  override fun run() {
+    val consumer = newConsumer(ByteArrayDeserializer::class, ByteArrayDeserializer::class)
+    val partitions = consumer.topicPartitions(topic, Duration.ofMinutes(1))
+
+    try {
+      val reader = encoding.reader(System.`in`)
+      while (reader.hasNext()) {
+        val bytes = reader.next()
+        val partition = Utils.toPositive(Utils.murmur2(bytes)) % partitions.size
+        println("$topic-$partition")
+      }
+    } catch (t: EOFException) {
+      // Ignore, we just terminated between hasNext and next()
+    }
+  }
+}
