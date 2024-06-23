4f7f7455c308e50c801560f94555e408859bb834
Mark Green <markosindustries@gmail.com>
2024-06-23 13:43:19 +1000

    Upgrade to gradle 8.6 and fix some issues with the previous upgrade
    Notable, move from merged single JAR output to shipping JARs separately

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ die() { echo "$*" 1>&2 ; exit 1; }
 
 echo $1 | grep -E -q '^([0-9]+).([0-9]+).([0-9]+)$' || die "Version must be of the form N.N.N, $1 provided"
 VERSION=$1
-docker build -f docker/Dockerfile \
+docker build -f docker/Dockerfile --pull \
  --build-arg MSG_VERSION="${VERSION}" \
  -t markosindustries/monosodium-glutamate:latest \
  -t markosindustries/monosodium-glutamate:$VERSION \
