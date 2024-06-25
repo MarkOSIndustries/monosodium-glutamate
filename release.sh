d53342273327934fc593f7889c173ceb56538841
Mark Green <markosindustries@gmail.com>
2024-06-26 00:33:13 +1000

    Add docker platform support for linux/arm64

--- release.sh
+++ release.sh
@@ -3,13 +3,18 @@ die() { echo "$*" 1>&2 ; exit 1; }
 
 echo $1 | grep -E -q '^([0-9]+).([0-9]+).([0-9]+)$' || die "Version must be of the form N.N.N, $1 provided"
 VERSION=$1
-docker build -f docker/Dockerfile --pull \
- --build-arg MSG_VERSION="${VERSION}" \
- -t markosindustries/monosodium-glutamate:latest \
- -t markosindustries/monosodium-glutamate:$VERSION \
- .
 
+# docker buildx create --name=multi-platform-builder --driver=docker-container --use --bootstrap
+
+BUILD_COMMAND="build"
 if [[ $2 == "push" ]]; then
-  docker push markosindustries/monosodium-glutamate:latest
-  docker push markosindustries/monosodium-glutamate:$VERSION
+  BUILD_COMMAND="buildx build --platform linux/amd64,linux/arm64 --push"
 fi
+
+docker $BUILD_COMMAND \
+  -f docker/Dockerfile \
+  --pull \
+  --build-arg MSG_VERSION="${VERSION}" \
+  -t markosindustries/monosodium-glutamate:latest \
+  -t markosindustries/monosodium-glutamate:$VERSION \
+  .
